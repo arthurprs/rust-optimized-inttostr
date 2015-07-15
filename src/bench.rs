0930252b8c0849f925a0a23f8ef6d617b7a8e7ad
arthurprs <arthurprs@gmail.com>
2015-07-15 00:32:04 -0300

    added str to int opt

--- src/bench.rs
+++ src/bench.rs
@@ -1,7 +1,5 @@
-
-
-// use strconv::int2dec::UintToDec;
 use Wrapper;
+use std::str::FromStr;
 use std::fmt::Write as fmtWrite;
 use std::io::{self, Write};
 use test;
@@ -99,3 +97,55 @@ bench_stdlib_skewed!(u8: skewed_h_stdlib_u08, 2000, 0.1f64);
 bench_stdlib_skewed!(u16: skewed_h_stdlib_u16, 10000, 0.1f64);
 bench_stdlib_skewed!(u32: skewed_h_stdlib_u32, 100000, 0.1f64);
 bench_stdlib_skewed!(u64: skewed_h_stdlib_u64, 100000, 0.1f64);
+
+
+macro_rules! bench_from_str_std {
+	($t:ty: $name:ident, $samples:expr) => {
+		#[bench]
+		fn $name(b: &mut test::Bencher) {
+			let mut rng: StdRng = SeedableRng::from_seed(SEED);
+			let items: Vec<_> = (0..$samples).map(|_| format!("{}", rng.gen::<$t>()) ).collect();
+			b.iter(|| {
+				for str_ in &items {
+					let x: Result<$t, _> = FromStr::from_str(str_);
+					test::black_box(x);
+				}
+			});
+		}
+	};
+}
+
+macro_rules! bench_from_str_new {
+	($t:ty: $name:ident, $samples:expr) => {
+		#[bench]
+		fn $name(b: &mut test::Bencher) {
+			let mut rng: StdRng = SeedableRng::from_seed(SEED);
+			let items: Vec<_> = (0..$samples).map(|_| format!("{}", rng.gen::<$t>()) ).collect();
+			b.iter(|| {
+				for str_ in &items {
+					let x: Result<Wrapper<$t>, _> = FromStr::from_str(str_);
+					test::black_box(x);
+				}
+			});
+		}
+	};
+}
+
+
+bench_from_str_std!(u8: bench_from_str_std_u08, 20);
+bench_from_str_std!(u16: bench_from_str_std_u16, 20);
+bench_from_str_std!(u32: bench_from_str_std_u32, 20);
+bench_from_str_std!(u64: bench_from_str_std_u64, 20);
+bench_from_str_std!(i8: bench_from_str_std_i08, 20);
+bench_from_str_std!(i16: bench_from_str_std_i16, 20);
+bench_from_str_std!(i32: bench_from_str_std_i32, 20);
+bench_from_str_std!(i64: bench_from_str_std_i64, 20);
+
+bench_from_str_new!(u8: bench_from_str_new_u08, 20);
+bench_from_str_new!(u16: bench_from_str_new_u16, 20);
+bench_from_str_new!(u32: bench_from_str_new_u32, 20);
+bench_from_str_new!(u64: bench_from_str_new_u64, 20);
+bench_from_str_new!(i8: bench_from_str_new_i08, 20);
+bench_from_str_new!(i16: bench_from_str_new_i16, 20);
+bench_from_str_new!(i32: bench_from_str_new_i32, 20);
+bench_from_str_new!(i64: bench_from_str_new_i64, 20);
--- src/wrapper.rs
+++ src/wrapper.rs
@@ -1,4 +1,4 @@
-use std::fmt::{self, Display};
+use std::fmt::{self, Debug, Display};
 use std::mem;
 use std::num::Wrapping;
 use std::str;
@@ -20,7 +20,8 @@ macro_rules! impl_AsUInt {
 
 impl_AsUInt! { i8, i16, i32, i64, isize, u8, u16, u32, u64, usize }
 
-pub struct Wrapper<T>(pub T);
+#[derive(Debug, Copy, Clone, PartialEq, PartialOrd)]
+pub struct Wrapper<T: Debug + Copy + PartialEq + PartialOrd>(pub T);
 
 const DEC_DIGITS_LUT: [u8; 200] = [
     '0' as u8,'0' as u8,'0' as u8,'1' as u8,'0' as u8,'2' as u8,'0' as u8,'3' as u8,'0' as u8,'4' as u8,'0' as u8,'5' as u8,'0' as u8,'6' as u8,'0' as u8,'7' as u8,'0' as u8,'8' as u8,'0' as u8,'9' as u8,
@@ -102,3 +103,247 @@ impl_Display!(isize, usize: as_u32);
 #[cfg(target_pointer_width = "64")]
 impl_Display!(isize, usize: as_u64);
 
+/// An error which can be returned when parsing an integer.
+#[derive(Debug, Clone, PartialEq)]
+pub struct ParseIntError { kind: IntErrorKind }
+
+#[derive(Debug, Clone, PartialEq)]
+enum IntErrorKind {
+    Empty,
+    InvalidDigit,
+    Overflow,
+    Underflow,
+}
+
+
+#[doc(hidden)]
+trait FromStrRadixHelper: Copy + PartialOrd {
+    fn min_value() -> Self;
+    fn from_u32(u: u32) -> Self;
+    fn checked_mul(&self, other: u32) -> Option<Self>;
+    fn checked_sub(&self, other: u32) -> Option<Self>;
+    fn checked_add(&self, other: u32) -> Option<Self>;
+}
+
+// macro_rules! doit {
+//     ($($t:ty)*) => ($(impl FromStrRadixHelper for Wrapper<$t> {
+//         fn min_value() -> Self { Wrapper(<$t>::min_value()) }
+//         fn from_u32(u: u32) -> Self { Wrapper(u as $t) }
+//         fn checked_mul(&self, other: u32) -> Option<Self> {
+//             unsafe { mem::transmute(self.0.checked_mul(other as $t)) }
+//         }
+//         fn checked_sub(&self, other: u32) -> Option<Self> {
+//             unsafe { mem::transmute(self.0.checked_sub(other as $t)) }
+//         }
+//         fn checked_add(&self, other: u32) -> Option<Self> {
+//             unsafe { mem::transmute(self.0.checked_add(other as $t)) }
+//         }
+//     })*)
+// }
+// doit! { i8 i16 i32 i64 isize u8 u16 u32 u64 usize }
+
+macro_rules! doit {
+    ($($t:ty)*) => ($(impl FromStrRadixHelper for $t {
+        fn min_value() -> Self { Self::min_value() }
+        fn from_u32(u: u32) -> Self { u as Self }
+        fn checked_mul(&self, other: u32) -> Option<Self> {
+            Self::checked_mul(*self, other as Self)
+        }
+        fn checked_sub(&self, other: u32) -> Option<Self> {
+            Self::checked_sub(*self, other as Self)
+        }
+        fn checked_add(&self, other: u32) -> Option<Self> {
+            Self::checked_add(*self, other as Self)
+        }
+    })*)
+}
+doit! { i8 i16 i32 i64 isize u8 u16 u32 u64 usize }
+
+
+fn from_str_radix<T: FromStrRadixHelper>(src: &str, radix: u32)
+                                         -> Result<T, ParseIntError> {
+ 	const MINUS: u8 = '-' as u8;
+    use self::IntErrorKind::*;
+    use self::ParseIntError as PIE;
+
+    assert!(radix >= 2 && radix <= 36,
+           "from_str_radix_int: must lie in the range `[2, 36]` - found {}",
+           radix);
+
+    if src.is_empty() {
+    	return Err(PIE { kind: Empty });
+    }
+
+    let is_signed_ty = T::from_u32(0) > T::min_value();
+    // all valid digits are ascii
+    let src = src.as_bytes();
+
+    match (src[0], &src[1..])  {
+        (MINUS, src) if src.is_empty() => Err(PIE { kind: Empty }),
+        (MINUS, src) if is_signed_ty => {
+            // The number is negative
+            let mut result = T::from_u32(0);
+            for &c in src {
+                let x = match (c as char).to_digit(radix) {
+                    Some(x) => x,
+                    None => return Err(PIE { kind: InvalidDigit }),
+                };
+                result = match result.checked_mul(radix) {
+                    Some(result) => result,
+                    None => return Err(PIE { kind: Underflow }),
+                };
+                result = match result.checked_sub(x) {
+                    Some(result) => result,
+                    None => return Err(PIE { kind: Underflow }),
+                };
+            }
+            Ok(result)
+        },
+        (c, src) => {
+            // The number is signed
+            let mut result = match (c as char).to_digit(radix) {
+                Some(x) => T::from_u32(x),
+                None => return Err(PIE { kind: InvalidDigit }),
+            };
+            for &c in src {
+                let x = match (c as char).to_digit(radix) {
+                    Some(x) => x,
+                    None => return Err(PIE { kind: InvalidDigit }),
+                };
+                result = match result.checked_mul(radix) {
+                    Some(result) => result,
+                    None => return Err(PIE { kind: Overflow }),
+                };
+                result = match result.checked_add(x) {
+                    Some(result) => result,
+                    None => return Err(PIE { kind: Overflow }),
+                };
+            }
+            Ok(result)
+        }
+    }
+}
+
+impl<T> str::FromStr for Wrapper<T>
+		where T: FromStrRadixHelper + Debug + Copy + PartialEq + PartialOrd {
+	type Err = ParseIntError;
+	fn from_str(src: &str) -> Result<Self, ParseIntError> {
+        from_str_radix(src, 10).map(|i| Wrapper(i))
+    }
+}
+
+#[cfg(test)]
+mod from_str_tests {
+	use super::{Wrapper, IntErrorKind, ParseIntError};
+	use std::str::FromStr;
+	use rand::{self, Rng};
+
+	#[test]
+	fn test_from_str() {
+		assert_eq!(FromStr::from_str("11"), Ok(Wrapper(11)));
+		assert_eq!(FromStr::from_str("-11"), Ok(Wrapper(-11)));
+	}
+
+	#[test]
+	fn test_overflow() {
+		let conv: Result<Wrapper<i8>, _> = FromStr::from_str("-129");
+		assert_eq!(conv, Err(ParseIntError{ kind: IntErrorKind::Underflow }));
+		let conv: Result<Wrapper<i8>, _> = FromStr::from_str("128");
+		assert_eq!(conv, Err(ParseIntError{ kind: IntErrorKind::Overflow }));
+	}
+
+	#[test]
+	fn test_invalid() {
+		let conv: Result<Wrapper<i8>, _> = FromStr::from_str("--129");
+		assert_eq!(conv, Err(ParseIntError{ kind: IntErrorKind::InvalidDigit }));
+		let conv: Result<Wrapper<i8>, _> = FromStr::from_str("¼²¼");
+		assert_eq!(conv, Err(ParseIntError{ kind: IntErrorKind::InvalidDigit }));
+	}
+
+	#[test]
+	fn test_empty() {
+		let conv: Result<Wrapper<i8>, _> = FromStr::from_str("-");
+		assert_eq!(conv, Err(ParseIntError{ kind: IntErrorKind::Empty }));
+		let conv: Result<Wrapper<u8>, _> = FromStr::from_str("-");
+		assert_eq!(conv, Err(ParseIntError{ kind: IntErrorKind::Empty }));
+		let conv: Result<Wrapper<i8>, _> = FromStr::from_str("");
+		assert_eq!(conv, Err(ParseIntError{ kind: IntErrorKind::Empty }));
+		let conv: Result<Wrapper<u8>, _> = FromStr::from_str("");
+		assert_eq!(conv, Err(ParseIntError{ kind: IntErrorKind::Empty }));
+	}
+
+	macro_rules! test_from_str_type {
+		($t:ty: $name:ident) => {
+			#[test]
+			fn $name() {
+				let mut rnd = rand::weak_rng();
+				for _ in (0..1000000) {
+					let i: $t = rnd.gen();
+					assert_eq!(FromStr::from_str(&format!("{}", i)), Ok(Wrapper(i)));
+				}
+			}
+		}
+	}
+
+	test_from_str_type!(u8: test_from_str_u8);
+	test_from_str_type!(u16: test_from_str_u16);
+	test_from_str_type!(u32: test_from_str_u32);
+	test_from_str_type!(u64: test_from_str_u64);
+	test_from_str_type!(i8: test_from_str_i8);
+	test_from_str_type!(i16: test_from_str_i16);
+	test_from_str_type!(i32: test_from_str_i32);
+	test_from_str_type!(i64: test_from_str_i64);
+}
+
+
+#[cfg(test)]
+mod display_tests {
+	use super::Wrapper;
+	use rand::{self, Rng};
+
+	#[test]
+	fn test_pos() {
+		for i in (0i64..10000) {
+			let n = Wrapper(i);
+			assert_eq!(format!("{}", i), format!("{}", n));
+		}
+	}
+
+	#[test]
+	fn test_neg() {
+		for i in (0i64..10000) {
+			let n = Wrapper(-i);
+			assert_eq!(format!("{}", -i), format!("{}", n));
+		}
+	}
+
+	#[test]
+	fn test_overflow() {
+		assert_eq!("-128", format!("{}", Wrapper(-128i8)));
+		assert_eq!("-32768", format!("{}", Wrapper(-32768i16)));
+		assert_eq!("-2147483648", format!("{}", Wrapper(-2147483648i32)));
+		assert_eq!("-9223372036854775808", format!("{}", Wrapper(-9223372036854775808i64)));
+	}
+
+	macro_rules! test_display_type {
+		($t:ty: $name:ident) => {
+			#[test]
+			fn $name() {
+				let mut rnd = rand::weak_rng();
+				for _ in (0..1000000) {
+					let i: $t = rnd.gen();
+					assert_eq!(format!("{}", i), format!("{}", Wrapper(i)));
+				}
+			}
+		}
+	}
+
+	test_display_type!(u8: test_display_u8);
+	test_display_type!(u16: test_display_u16);
+	test_display_type!(u32: test_display_u32);
+	test_display_type!(u64: test_display_u64);
+	test_display_type!(i8: test_display_i8);
+	test_display_type!(i16: test_display_i16);
+	test_display_type!(i32: test_display_i32);
+	test_display_type!(i64: test_display_i64);
+}
